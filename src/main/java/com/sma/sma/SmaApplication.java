40bc26b32920823b5549eed10a8fae26d070c39d
Eliza <eliza-staszczak@wp.pl>
2021-11-29 16:31:10 +0100

    Finishing backend

--- src/main/java/com/sma/sma/SmaApplication.java
+++ src/main/java/com/sma/sma/SmaApplication.java
@@ -1,13 +1,28 @@
 package com.sma.sma;
 
+import com.sma.sma.enumeration.Status;
+import com.sma.sma.model.Server;
+import com.sma.sma.repo.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SmaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower", "http://localhost:8080/server/image/server2.png", Status.SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", Status.SERVER_DOWN));
+		};
 	}
 
 }
